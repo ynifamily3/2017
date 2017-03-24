68b771cfc1a31685ff614bbae70e953b8a21af1c
jongkeun choi <ynifamily3@gmail.com>
2017-03-24 04:55:35 +0000

    analytics

--- dist/app.js
+++ dist/app.js
@@ -125,6 +125,15 @@ __webpack_require__(0);
     cobj[1].addEventListener("click", fn2);
 })(window, document.getElementsByClassName("content__button"));
 
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments);
+    }, i[r].l = 1 * new Date();a = s.createElement(o), m = s.getElementsByTagName(o)[0];a.async = 1;a.src = g;m.parentNode.insertBefore(a, m);
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-75074154-1', 'auto');
+ga('send', 'pageview');
+
 /***/ }),
 /* 2 */
 /***/ (function(module, exports, __webpack_require__) {
--- src/app.js
+++ src/app.js
@@ -23,3 +23,11 @@ import './style.css';
     cobj[0].addEventListener("click", fn);
     cobj[1].addEventListener("click", fn2);
 })(window, document.getElementsByClassName("content__button"));
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-75074154-1', 'auto');
+  ga('send', 'pageview');
